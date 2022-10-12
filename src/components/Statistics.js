dabf68ffd8ef6b98c25761114a65c57f5d0b9cbb
nomanshakib <nomanshakib.official@gmail.com>
2022-10-12 14:31:56 +0600

    statistics initial

---
+++ src/components/Statistics.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div className='md:pt-36 flex justify-center flex-col max-w-[730px] m-auto'>
+            <h1 className="text-3xl text-gray-600 font-bold relative mb-10 quiz-title pb-5 leading-none text-center sm:text-4xl">
+                Quiz Summary!
+            </h1>
+        </div>
+    );
+};
+
+export default Statistics;
+
--- src/utilities/routes.js
+++ src/utilities/routes.js
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
 import Root from "../components/Root";
+import Statistics from "../components/Statistics";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
         children: [
-            {path: '/', element: <Home />},
+            { path: '/', element: <Home /> },
+            {path: '/statistics', element: <Statistics />},
         ]
     }
 ]);
