055c8b86ea9056eb32aedae917dba260f73c0bda
nomanshakib <nomanshakib.official@gmail.com>
2022-10-12 14:37:53 +0600

    404 error page link broken

--- src/utilities/routes.js
+++ src/utilities/routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage";
 import Home from "../components/Home";
 import Root from "../components/Root";
 import Statistics from "../components/Statistics";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
+        errorElement: <ErrorPage/>,
         children: [
             { path: '/', element: <Home /> },
             {path: '/statistics', element: <Statistics />},
